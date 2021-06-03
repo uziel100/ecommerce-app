ecfffd9f4f693673f78e5b3ae3ddb9eaf12e7dc8
Uziel Hernandez <uzielmelitonh@gmail.com>
2021-06-03 01:00:25 -0500

    Add navigation

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useMemo, useState } from "react";
 import { Text, View } from "react-native";
-import jwtDecode from 'jwt-decode';
+import jwtDecode from "jwt-decode";
+import AppNavigation from "./src/navigation/AppNavigation";
+
 import { Provider as PaperProvider, Button } from "react-native-paper";
 import { Auth as AuthScreen } from "./src/screens/Auth";
 
@@ -8,57 +10,54 @@ import AuthContext from "./src/context/AuthContext";
 import { setTokenApi, getTokenApi, delTokenApi } from "./src/api/token";
 import { layoutStyle } from "./src/styles";
 
-
 export default function App() {
     const [auth, setAuth] = useState(undefined);
 
     useEffect(() => {
-        ( async () => {
+        (async () => {
             const token = await getTokenApi();
-            if(token){                             
+            if (token) {
                 setAuth({
                     token,
-                    idUser:  jwtDecode( token ).id
-                })
-            }else{
-                setAuth(null)
+                    idUser: jwtDecode(token).id,
+                });
+            } else {
+                setAuth(null);
             }
-        })()
-    }, [])
+        })();
+    }, []);
 
-    const login = (user) => {        
-        setTokenApi( user.jwt );
+    const login = (user) => {
+        setTokenApi(user.jwt);
         setAuth({
             token: user.jwt,
-            idUser: user.user._id
-        })
-    }
+            idUser: user.user._id,
+        });
+    };
 
     const logout = () => {
-        if(auth){
-            delTokenApi()
+        if (auth) {
+            delTokenApi();
             setAuth(null);
         }
-    }
+    };
 
-    const authData = useMemo(() => ({
-        auth,
-        login,
-        logout,
-    }) , [auth]);
+    const authData = useMemo(
+        () => ({
+            auth,
+            login,
+            logout,
+        }),
+        [auth]
+    );
 
-    if( auth === undefined ) return null;
+    if (auth === undefined) return null;
 
     return (
-        <AuthContext.Provider value={ authData } >
+        <AuthContext.Provider value={authData}>
             <PaperProvider>
-                {auth ? 
-                <View style={ layoutStyle.container } >
-                    <Text>Zona de usuarios</Text>
-                    <Button onPress={ authData.logout } mode="outlined">Cerrar sesion</Button>
-                </View> : <AuthScreen />}
+                {auth ? <AppNavigation /> : <AuthScreen />}
             </PaperProvider>
         </AuthContext.Provider>
     );
 }
-
