62345e8497d46e1f333eaa94123118ebf2ddde4c
uziel100 <uziemelitonh@gmail.com>
2021-06-03 12:22:26 -0500

    Config nav stack

---
+++ src/components/Search/Search.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+import { Searchbar } from "react-native-paper";
+import colors from '../../styles/colors'
+
+export default function Search() {
+    return (
+        <View style={ styles.container }>
+            <Searchbar 
+                placeholder='Buscar...'
+            />
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor: colors.bgDark,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        zIndex: 1
+    }
+})
--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { useFocusEffect } from "@react-navigation/core";
+import React, { useCallback } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { getMeApi } from "../api/user";
+import useAuth from "../hooks/useAuth";
+import Search from "../components/Search";
+import StatusBarCustom from "../components/StatusBar";
+import colors from "../styles/colors";
 
 export default function Account() {
+    const { auth } = useAuth();
+
+    useFocusEffect(
+        useCallback(() => {
+            (async () => {
+                const response = await getMeApi(auth.token);
+                console.log(response);
+            })();
+        }, [])
+    );
+
     return (
-        <View style={ styles.container }>
-            <Text>Welcome to Account</Text>
-        </View>
-    )
+        <>
+            <StatusBarCustom
+                backgroundColor={colors.bgDark}
+                barStyle="light-content"
+            />
+            <Search />
+            <ScrollView>
+                <Text>Welcome to Account</Text>
+            </ScrollView>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center', 
-        alignItems: 'center'       
-    }
-})
+    container: {},
+});
